e4b6401096ac289abb4eeac222ff70203247f885
anpdko <anpdko@gmail.com>
2023-08-27 14:53:37 +0300

    try-catch reviews router

--- routes/reviews.route.js
+++ routes/reviews.route.js
@@ -5,12 +5,18 @@ const { verifyToken } = require('../middleware/admin.middleware');
 
 // GET api/reviews - Get all reviews
 router.get('/', (req, res) => {
-  Reviews.find()
-    .then(reviews =>{
-      res.json(reviews)
-    })
-    .catch(err => {
-      res.status(404).json({ msg: 'Reviews not found' })});
+  try {
+    Reviews.find()
+      .then(reviews =>{
+        res.json(reviews)
+      })
+      .catch(err => {
+        res.status(404).json({ message: 'Reviews not found' })});
+  }
+  catch(err) {
+    console.log(err);
+    res.status(404).json({ err, message: 'Server Error' });
+  }
 });
 
 // GET api/reviews/:id - Get one reviews by id
@@ -21,32 +27,32 @@ router.get('/:id', (req, res) => {
       console.log("reviews:", reviews)
       res.json(reviews)
     })
-    .catch(err => res.status(404).json({ msg: 'Reviews not found' }));
+    .catch(err => res.status(404).json({ message: 'Reviews not found' }));
 });
 
 // GET api/reviews - Add/save reviews
 router.post('/', verifyToken, (req, res) => {
   Reviews.create(req.body)
     .then(reviews => {
-      res.json({ msg: 'Reviews added successfully' })
+      res.json({ message: 'Reviews added successfully' })
     })
-    .catch(err => res.status(400).json({ error: 'Failed to add reviews' }));
+    .catch(err => res.status(400).json({ message: 'Failed to add reviews' }));
 });
 
 // GET api/reviews/:id - Update reviews
 router.put('/:id', verifyToken, (req, res) => {
   Reviews.findByIdAndUpdate(req.params.id, req.body)
-    .then(reviews => res.json({ msg: 'Successfully updated' }))
+    .then(reviews => res.json({ message: 'Successfully updated' }))
     .catch(err =>
-      res.status(400).json({ error: 'Unable to update database' })
+      res.status(400).json({ message: 'Unable to update database' })
     );
 });
 
 // GET api/reviews/:id - Delete reviews
 router.delete('/:id', verifyToken, (req, res) => {
   Reviews.findByIdAndRemove(req.params.id, req.body)
-    .then(reviews => res.json({ mgs: 'Reviews successfully deleted' }))
-    .catch(err => res.status(404).json({ error: 'News not found' }));
+    .then(reviews => res.json({ message: 'Reviews successfully deleted' }))
+    .catch(err => res.status(404).json({ message: 'News not found' }));
 });
 
 module.exports = router;
