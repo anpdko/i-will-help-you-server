4643793f5463b2a95a18cb65573c867d84b58c91
anpdko <anpdko@gmail.com>
2023-08-31 00:36:09 +0300

    fix imgs data

--- routes/admin.route.js
+++ routes/admin.route.js
@@ -53,7 +53,7 @@ router.post('/login', async (req, res) => {
       if (!admin) {
          return res.status(400).json({
             message: {
-               login: 'Incorrect data during registration'
+               login: 'Incorrect login. Please try again'
             }
          })
       }
@@ -63,7 +63,7 @@ router.post('/login', async (req, res) => {
       if (!isMatch) {
          return res.status(400).json({
             message: {
-               password: 'Incorrect data during registration!'
+               password: 'Incorrect password. Please try again'
             }
          })
       }
