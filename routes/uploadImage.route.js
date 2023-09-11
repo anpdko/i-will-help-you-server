09e2b0cad1c38709b0f294c163abeb37df465d10
PavlenkoDm <dmpavlenko07@gmail.com>
2023-09-11 20:34:54 +0300

    add verifyToken to image-upload

--- routes/uploadImage.route.js
+++ routes/uploadImage.route.js
@@ -5,7 +5,13 @@ const router = express.Router();
 const { uploadImage } = require("../controllers/uploadFiles");
 const { controllerWrapper } = require("../helpers");
 const { upload } = require("../middleware/file.middeleware");
+const { verifyToken } = require("../middleware/admin.middleware");
 
-router.post("/", upload.single("image"), controllerWrapper(uploadImage));
+router.post(
+  "/",
+  verifyToken,
+  upload.single("image"),
+  controllerWrapper(uploadImage)
+);
 
 module.exports = router;
