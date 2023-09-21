84308323ca34d8811310bdd66f5fe094287da77d
anpdko <anpdko@gmail.com>
2023-09-21 13:01:43 +0300

    fix pay

--- routes/payment.route.js
+++ routes/payment.route.js
@@ -1,40 +1,40 @@
 const express = require("express");
 const router = express.Router();
 
-// const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY, {
-//   apiVersion: "2022-08-01",
-// });
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY, {
+  apiVersion: "2022-08-01",
+});
 
-// stripe.applePayDomains.create({
-//   domain_name: "i-will-help-you.netlify.app",
-// });
+stripe.applePayDomains.create({
+  domain_name: "i-will-help-you.netlify.app",
+});
 
-// router.get("/config", (req, res) => {
-//   res.send({
-//     publishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
-//   });
-// });
+router.get("/config", (req, res) => {
+  res.send({
+    publishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
+  });
+});
 
-// router.post("/create-payment-intent", async (req, res) => {
-//   try {
-//     const { email, amount } = req.body;
-//     console.log(email, Number(amount) * 100);
-//     const paymentIntent = await stripe.paymentIntents.create({
-//       currency: "usd",
-//       amount: Number(amount) * 100,
-//       automatic_payment_methods: { enabled: true },
-//     });
+router.post("/create-payment-intent", async (req, res) => {
+  try {
+    const { email, amount } = req.body;
+    console.log(email, Number(amount) * 100);
+    const paymentIntent = await stripe.paymentIntents.create({
+      currency: "usd",
+      amount: Number(amount) * 100,
+      automatic_payment_methods: { enabled: true },
+    });
 
-//     res.send({
-//       clientSecret: paymentIntent.client_secret,
-//     });
-//   } catch (e) {
-//     return res.status(400).send({
-//       error: {
-//         message: e.message,
-//       },
-//     });
-//   }
-// });
+    res.send({
+      clientSecret: paymentIntent.client_secret,
+    });
+  } catch (e) {
+    return res.status(400).send({
+      error: {
+        message: e.message,
+      },
+    });
+  }
+});
 
 module.exports = router;
