65def8b6cfba994f909808470c15312e311de0ae
anpdko <anpdko@gmail.com>
2023-09-29 15:54:16 +0300

    test upload

--- routes/upload.route.js
+++ routes/upload.route.js
@@ -1,39 +1,49 @@
 const express = require("express");
 const router = express.Router();
-const { Blob, createBlobReadStream, createBlobWriteStream } = require('@vercel/blob');
+const {
+  Blob,
+  createBlobReadStream,
+  createBlobWriteStream,
+} = require("@vercel/blob");
 
-const uploadTest = () => {
+// Ваш токен для чтения и записи
+const { BLOB_READ_WRITE_TOKEN } = process.env;
 
-}
+router.get("/read/:key", async (req, res) => {
+  const { key } = req.params;
 
-router.post('/', async (req, res) => {
-   try {
-     const blob = new Blob();
-     const writeStream = createBlobWriteStream(blob);
- 
-     req.pipe(writeStream);
- 
-     writeStream.on('finish', () => {
-       res.status(200).json({ success: true, url: blob.dataURL });
-     });
-   } catch (error) {
-     console.error(error);
-     res.status(500).json({ success: false, error: 'Ошибка загрузки файла' });
-   }
- });
+  try {
+    const blobReadStream = createBlobReadStream({
+      token: BLOB_READ_WRITE_TOKEN,
+      key,
+    });
 
- // Получение файла с сервера
- router.get('/:blobId', async (req, res) => {
-   try {
-     const blob = new Blob({ id: req.params.blobId });
-     const readStream = createBlobReadStream(blob);
- 
-     readStream.pipe(res);
-   } catch (error) {
-     console.error(error);
-     res.status(500).json({ success: false, error: 'Ошибка получения файла' });
-   }
- });
+    blobReadStream.pipe(res);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Произошла ошибка при чтении файла" });
+  }
+});
 
+router.post("/write/:key", async (req, res) => {
+  const { key } = req.params;
+  const data = req.body;
+
+  try {
+    const blobWriteStream = createBlobWriteStream({
+      token: BLOB_READ_WRITE_TOKEN,
+      key,
+    });
+
+    blobWriteStream.end(data);
+
+    blobWriteStream.on("finish", () => {
+      res.status(200).json({ message: "Файл успешно записан" });
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Произошла ошибка при записи файла" });
+  }
+});
 
 module.exports = router;
